9d3f47957921c8858cd4b26c1b4e7d10790cfd0d
fmg-team <devteam@futuremedia.co.za>
2021-09-22 15:14:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-1kg7sx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-1kg7sx)
---
+++ index.js
@@ -0,0 +1,61 @@
+// run `node index.js` in the terminal
+const axios = require('axios').default;
+// [x] basic js (es6)
+/*
+  iife,
+  destructuring,
+  spread,
+  optional chaining,
+  async/await
+  sort
+  let/const
+*/
+// [x] add api
+// [ ] add s3
+// [ ] add db (mongo test)
+
+const url1 =
+  'https://stormersmagazine.com/api/publications?isPreview=false&issuePermalink=issue-91&permalink=stormersmagazine.com';
+const url2 =
+  'https://springbokmagazine.com/api/publications?isPreview=false&permalink=springbokmagazine.com';
+
+(async function () {
+  let stormersPages = await getPublicationPages(url1);
+  let springbokPages = await getPublicationPages(url2);
+
+  stormersPages = stormersPages.map(({ title }, index) => {
+    return {
+      title,
+      index,
+      type: 'stormersPages',
+    };
+  });
+  springbokPages = springbokPages.map(({ title }, index) => {
+    return {
+      title,
+      index,
+      type: 'springbokPages',
+    };
+  });
+  let allPages = [...stormersPages, ...springbokPages];
+  allPages = allPages.sort((a, b) => {
+    console.log(
+      `${a.title} length > ${b.title} length : ${
+        a.title.length > b.title.length
+      }`
+    );
+    return a.title.length > b.title.length ? 1 : -1;
+  });
+  allPages.forEach((p, i) => {
+    console.log(`Page ${p.index + 1} of ${p.type}: ${p.title}`);
+  });
+})();
+
+async function getPublicationPages(url) {
+  try {
+    const res = await axios.get(url);
+    return res?.data?.issues[0]?.pages;
+  } catch (e) {
+    console.error(e.message);
+  }
+}
